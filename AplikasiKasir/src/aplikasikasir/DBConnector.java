410e08580912f6b07d40b817c280d20fdad28f85
unfocusedlex <92197888+unfocusedlex@users.noreply.github.com>
2023-04-21 11:02:36 +0700

    Update DBConnector.java

--- AplikasiKasir/src/aplikasikasir/DBConnector.java
+++ AplikasiKasir/src/aplikasikasir/DBConnector.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package aplikasikasir;
-
+//testign add
 import java.sql.Connection;
 import java.sql.DriverManager;
 
