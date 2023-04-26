dce9aef066d1d13d133c348a890e70de0c3a22ec
unfocusedlex <92197888+unfocusedlex@users.noreply.github.com>
2023-04-26 23:21:35 +0700

    Update database_update.sql

--- database_update.sql
+++ database_update.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2023 at 04:51 PM
+-- Generation Time: Apr 26, 2023 at 06:21 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -24,6 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `kode` smallint UNSIGNED NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `harga` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`kode`, `nama`, `harga`) VALUES
+(1111, 'Chitato', 10000),
+(1112, 'Ginjal', 20000),
+(1114, 'Hati', 69000),
+(1115, 'Lays', 12000),
+(1116, 'Indomie', 3000),
+(1117, 'Popmie', 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_in_transaksi`
+--
+
+CREATE TABLE `item_in_transaksi` (
+  `kode` smallint UNSIGNED NOT NULL,
+  `jumlah` int NOT NULL,
+  `no_transaksi` tinyint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `item_in_transaksi`
+--
+
+INSERT INTO `item_in_transaksi` (`kode`, `jumlah`, `no_transaksi`) VALUES
+(1111, 9, 2),
+(1111, 6, 3),
+(1112, 4, 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
@@ -32,15 +77,78 @@ CREATE TABLE `login` (
   `password_hash` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `no` tinyint NOT NULL,
+  `totalBelanja` float NOT NULL,
+  `dibayar` float NOT NULL,
+  `kembalian` float NOT NULL,
+  `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`no`, `totalBelanja`, `dibayar`, `kembalian`, `waktu`) VALUES
+(1, 346000, 1500000, 1154000, '2023-04-26 02:38:18'),
+(2, 90000, 100000, 10000, '2023-04-26 02:39:15'),
+(3, 140000, 150000, 10000, '2023-04-26 04:11:11');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kode`);
+
+--
+-- Indexes for table `item_in_transaksi`
+--
+ALTER TABLE `item_in_transaksi`
+  ADD KEY `item_ibfk_1` (`no_transaksi`),
+  ADD KEY `kode` (`kode`);
 
 --
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `no` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `item_in_transaksi`
+--
+ALTER TABLE `item_in_transaksi`
+  ADD CONSTRAINT `item_in_transaksi_ibfk_1` FOREIGN KEY (`no_transaksi`) REFERENCES `transaksi` (`no`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `item_in_transaksi_ibfk_2` FOREIGN KEY (`kode`) REFERENCES `barang` (`kode`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
